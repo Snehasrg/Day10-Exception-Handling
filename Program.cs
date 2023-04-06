6b5c5b83457104be480d70c876a5b3ee3dc5c043
Snehasrg <sghongade642@gmail.com>
2023-04-06 23:07:21 +0530

    greet

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using ExceptionHandling;
+
+namespace Day10_ExceptionHandling
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+   
+            Console.WriteLine("Enter your Age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            PersonCategorybasedoAge categorized =new PersonCategorybasedoAge();
+            categorized.GetCategory(age);
+        }
+    }
+}
