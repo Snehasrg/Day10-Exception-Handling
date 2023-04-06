410ea1f966cb36d0fad73ca376148aa22a14a280
Snehasrg <sghongade642@gmail.com>
2023-04-06 23:16:03 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ namespace Day10_ExceptionHandling
     {
         static void Main(string[] args)
         {
-   
+       
             Console.WriteLine("Enter your Age");
             int age = Convert.ToInt32(Console.ReadLine());
-            PersonCategorybasedoAge categorized =new PersonCategorybasedoAge();
+            PersonCategorybasedoAge categorized = new PersonCategorybasedoAge();
             categorized.GetCategory(age);
         }
     }
